a2d1bccbb391f1aad724da8a0de90ea2489cbc4d
Kishor Rathva <kishor.rathva@rapidops.com>
2019-02-25 18:41:09 +0530

    JSONAPI with express

---
+++ jsonAPIexpress/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+//USe cors module for enable Cross-origin resources sharing
+const cors = require('cors');
+
+var app = express();
+
+app.use(cors()); //for all routes
+
+var port =  process.env.PORT || 8080;
+
+app.get('/',(req,res) => {
+    var info = {
+        'string_value':'StackOverflow',
+        'number_value' :8476
+    }
+    res.json(info);
+    // res.status(200).json(info);
+    //or
+    // res.send(JSON.stringify({
+    //     'string_value':'StackOverflow',
+    //     'number_value' :8476
+    // }))
+})
+
+app.listen(port ,() => {
+    console.log(`Node.js listening on port ${port}`) 
+})
